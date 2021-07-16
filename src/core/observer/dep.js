08d1a4c5a94e041d0d824e30ce4a35c3003777a7
jessylinlin <1501103995@qq.com>
2021-07-16 19:30:58 +0800

    响应式原理

--- src/core/observer/dep.js
+++ src/core/observer/dep.js
@@ -11,56 +11,59 @@ let uid = 0
  * directives subscribing to it.
  */
 export default class Dep {
-  static target: ?Watcher;
-  id: number;
-  subs: Array<Watcher>;
+    static target: ? Watcher;
+    id: number;
+    subs: Array < Watcher > ;
 
-  constructor () {
-    this.id = uid++
-    this.subs = []
-  }
-
-  addSub (sub: Watcher) {
-    this.subs.push(sub)
-  }
-
-  removeSub (sub: Watcher) {
-    remove(this.subs, sub)
-  }
-
-  depend () {
-    if (Dep.target) {
-      Dep.target.addDep(this)
+    constructor() {
+        this.id = uid++
+            this.subs = []
     }
-  }
 
-  notify () {
-    // stabilize the subscriber list first
-    const subs = this.subs.slice()
-    if (process.env.NODE_ENV !== 'production' && !config.async) {
-      // subs aren't sorted in scheduler if not running async
-      // we need to sort them now to make sure they fire in correct
-      // order
-      subs.sort((a, b) => a.id - b.id)
+    addSub(sub: Watcher) {
+        this.subs.push(sub)
     }
-    for (let i = 0, l = subs.length; i < l; i++) {
-      subs[i].update()
+
+    removeSub(sub: Watcher) {
+        remove(this.subs, sub)
     }
-  }
+
+    depend() {
+        if (Dep.target) {
+            Dep.target.addDep(this)
+        }
+    }
+
+    notify() {
+        // stabilize the subscriber list first
+        const subs = this.subs.slice()
+        if (process.env.NODE_ENV !== 'production' && !config.async) {
+            // subs aren't sorted in scheduler if not running async
+            // we need to sort them now to make sure they fire in correct
+            // order
+            subs.sort((a, b) => a.id - b.id)
+        }
+        for (let i = 0, l = subs.length; i < l; i++) {
+            //Watcher对象
+            subs[i].update()
+        }
+    }
 }
-
+//存储当前正在执行的目标watcher对象
+//同一时间只有一个watcher对象使用
 // The current target watcher being evaluated.
 // This is globally unique because only one watcher
 // can be evaluated at a time.
 Dep.target = null
 const targetStack = []
 
-export function pushTarget (target: ?Watcher) {
-  targetStack.push(target)
-  Dep.target = target
+export function pushTarget(target: ? Watcher) {
+    // 当前的watcher保存到栈中， 赋值给dep.target
+    targetStack.push(target)
+    Dep.target = target
 }
 
-export function popTarget () {
-  targetStack.pop()
-  Dep.target = targetStack[targetStack.length - 1]
+export function popTarget() {
+    targetStack.pop()
+    Dep.target = targetStack[targetStack.length - 1]
 }
